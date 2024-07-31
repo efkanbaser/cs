aee8de8c4df0c74357a55a5061eddef12b8693b9
Efkan Başer <118986513+efkanbaser@users.noreply.github.com>
2024-07-31 20:38:59 +0300

    Add files via upload

---
+++ operators.cs
@@ -0,0 +1,19 @@
+﻿// +, -, *, /, %
+// <, >, <=, >=, ==, !=
+
+int sell = 1;
+
+string str1 = "a";
+string str2 = "b";
+// bool b = str1 > str2; gives an error because you can't compare if strings are bigger than eachother or not
+
+char c1 = 'A';
+char c2 = 'b';
+bool b = c1 > c2;
+// this works because all char values have their respective ASCII values
+
+int code = Convert.ToInt32(c1);
+Console.WriteLine(code);
+
+
+
