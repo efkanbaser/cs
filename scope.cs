94186f0be1e358ca4a63f9ed9e3f969da516eac3
Efkan Başer <118986513+efkanbaser@users.noreply.github.com>
2024-07-31 20:04:51 +0300

    Add files via upload

---
+++ scope.cs
@@ -0,0 +1,25 @@
+﻿
+// you can't write code in namespace scope
+
+public class DegiskenlerinYasamAlani
+{
+    // In class scope you can write variables and give them a value but cannot use them
+    int a = 15;
+    // int b = a + 10; doesn't work because you can't use int a in class scope
+
+    int b;
+    // if a variable is defined between class scope, it'll take an automatic value. 
+    // int = 0
+    // str = ""
+    // char = '' 
+    // etc...
+
+    // int k = b + 1; gives an error in this scope
+    
+    public void Main()
+    {
+        int k = b + 1; // doesn't give an error in this scope because b == 0
+        Console.WriteLine(k);
+    }
+
+}
