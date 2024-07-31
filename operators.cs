820311bf7e6d64cfcc25a2e79878f8af39f9ae10
Efkan Başer <118986513+efkanbaser@users.noreply.github.com>
2024-07-31 21:17:38 +0300

    Add files via upload

--- operators.cs
+++ operators.cs
@@ -1,19 +1,44 @@
-﻿// +, -, *, /, %
-// <, >, <=, >=, ==, !=
+﻿// Mathematical operators
+// +, -, *, /, %
 
-int sell = 1;
+
+// Comparison operators
+// <, >, <=, >=, ==, !=
+int num1 = 2;
+int num2 = 3;
+bool b1 = num1 > num2;
 
 string str1 = "a";
 string str2 = "b";
 // bool b = str1 > str2; gives an error because you can't compare if strings are bigger than eachother or not
 
-char c1 = 'A';
+char c1 = 'a';
 char c2 = 'b';
-bool b = c1 > c2;
+bool b2 = c1 > c2;
 // this works because all char values have their respective ASCII values
 
-int code = Convert.ToInt32(c1);
-Console.WriteLine(code);
+int code = Convert.ToInt32(c1); // To learn ASCII value of 'a'
 
 
+// Logical operators
+// &&, ||, !, &, |
+
+// && single false results in false
+// || single true results in true
+
+// & and | are used for comparisons between bits (0s and 1s) because it keeps looking even after a false value,
+// in practical coding this makes it work slower but when comparing bits it gives you the desired result wheras
+// double operands would give unwanted results
+
+bool bb = false;
+bool cc = !bb;
+
+
+// Increment, decrement
+// ++, --
+
+int sayi = 10;
+// sayi++; ++sayi; sayi--; --sayi;
+int sonuc = sayi++; // 10
+sonuc = ++sayi; // 11
 
