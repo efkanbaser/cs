175e003b408d9fc8d914e4c7efda26270b32bcc8
Efkan Başer <118986513+efkanbaser@users.noreply.github.com>
2024-08-12 19:54:47 +0300

    Update createInstance.cs

--- createInstance.cs
+++ createInstance.cs
@@ -5,3 +5,8 @@
 // Instance
 // Object
 // And append it to rnd
+
+int res1 = rnd.Next();
+int res2 = rnd.Next(500);
+int res3 = rnd.Next(500, 700);
+double res4 = rnd.NextDouble(); // 0-1 arasında 1 hariç bir sayı üretir
