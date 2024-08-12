ead8ee8e556c15e9292f036e4c15a9e9c07ac3c5
Efkan Başer <118986513+efkanbaser@users.noreply.github.com>
2024-08-12 19:52:46 +0300

    Add files via upload

---
+++ createInstance.cs
@@ -0,0 +1,7 @@
+﻿Random rnd = new Random();
+// First, create space in memory for random and name it 'rnd'
+// Then, from the existing class of Random create a
+// Copy
+// Instance
+// Object
+// And append it to rnd
