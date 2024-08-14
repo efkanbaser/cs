f7535f42af0f6e3e145d9c9d3ad44b189c1311e9
Efkan Başer <118986513+efkanbaser@users.noreply.github.com>
2024-08-14 20:57:49 +0300

    Add files via upload

---
+++ yildizlar.cs
@@ -0,0 +1,15 @@
+﻿string yildizlar = "";
+for (int i = 1; i < 7; i++)
+{
+    yildizlar += "*\n";
+    for (int j = 1; j < i+1; j++)
+    {
+        yildizlar += "*";
+        if (i == 6 && j==6)
+        {
+            yildizlar += "*";
+            break;
+        }
+    }
+}
+Console.WriteLine(yildizlar);
