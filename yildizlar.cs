ac753fd5c57be3bfaa990261ecc467bae62d0ba4
Efkan Başer <118986513+efkanbaser@users.noreply.github.com>
2024-08-14 21:08:03 +0300

    Add files via upload

--- yildizlar.cs
+++ yildizlar.cs
@@ -4,10 +4,10 @@ int kacSatir = 7;
 for (int i = 1; i < kacSatir; i++)
 {
     yildizlar += "*\n";
-    for (int j = 1; j < i+1; j++)
+    for (int j = 1; j < i + 1; j++)
     {
         yildizlar += "*";
-        if (i == 6 && j==6)
+        if (i == 6 && j == 6)
         {
             yildizlar += "*";
             break;
@@ -15,15 +15,33 @@ for (int i = 1; i < kacSatir; i++)
     }
 }
 Console.WriteLine(yildizlar);
+Console.WriteLine("\n");
 
-﻿string yildizlar1 = "";
+//-------------------------------------------------------------------------------------------
+string yildizlar1 = "";
 for (int i = 0; i < kacSatir; i++)
 {
     for (int j = 0; j < i; j++)
     {
         yildizlar1 += "*";
-  
+
     }
     yildizlar1 += "*\n";
 }
 Console.WriteLine(yildizlar1);
+Console.WriteLine("\n");
+
+//-------------------------------------------------------------------------------------------
+string yildizlar2 = "";
+
+for (int i = 0; i < kacSatir; i++)
+{
+    for (int j = 0; j < kacSatir - i-1; j++)
+    {
+        yildizlar2 += "*";
+        
+    }
+    yildizlar2 += "*\n";
+}
+Console.WriteLine(yildizlar2);
+Console.WriteLine("\n");
