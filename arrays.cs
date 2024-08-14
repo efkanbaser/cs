01b8d9eb43a0e340b6376c727935c45cdb660a11
Efkan Başer <118986513+efkanbaser@users.noreply.github.com>
2024-08-14 21:18:00 +0300

    Add files via upload

---
+++ arrays.cs
@@ -0,0 +1,18 @@
+﻿int[] sayilar = new int[4];
+// sayilar[0];
+// sayilar[1];
+// sayilar[2];
+// sayilar[3];
+
+sayilar[0] = 5;
+sayilar[1] = 14;
+sayilar[2] = 19;
+sayilar[3] = 9;
+
+// sayilar[4] = 10;
+// index out of bounds gives runtime error
+
+foreach (int num in sayilar)
+{
+    Console.WriteLine(num);
+}
