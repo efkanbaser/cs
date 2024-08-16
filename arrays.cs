c8d4e9284eaa909160c976f3db63c181b47d933b
Efkan Başer <118986513+efkanbaser@users.noreply.github.com>
2024-08-16 19:05:52 +0300

    Add files via upload

--- arrays.cs
+++ arrays.cs
@@ -1,9 +1,7 @@
-﻿int[] sayilar = new int[4];
-// sayilar[0];
-// sayilar[1];
-// sayilar[2];
-// sayilar[3];
+﻿using System.Collections;
 
+// 1st way of defining arrays
+int[] sayilar = new int[4];
 sayilar[0] = 5;
 sayilar[1] = 14;
 sayilar[2] = 19;
@@ -16,3 +14,32 @@ foreach (int num in sayilar)
 {
     Console.WriteLine(num);
 }
+
+// One disadv. of arrays is that you have to specify the amount of items it's going to contain while defining them.
+// One adv. of arrays that it's type safe
+
+
+// 2nd way of defining arrays
+int[] sayilar1 = new int[] { 3, 4, 5, 1, 6, 12, 6, 4, 2, 13 };
+int[] sayilar2;
+sayilar2 = new int[] { 3, 4, 5, 1, 6, 12, 6, 4, 2, 13 };
+
+
+// 3rd way of defining arrays
+int[] sayilar3 = { 2, 3, 4, 1, 2, 3, 5, 6, 1, 6 }; // Anonymous type
+// int[] sayilar4; 
+// sayilar4 = { 2, 3, 4, 1, 2, 3, 5, 6, 1, 6 }; this doesn't work
+
+
+
+
+
+
+
+// This is a non-generic list.
+ArrayList list = new ArrayList();
+list.Add(10);
+list.Add("Efkan");
+list.Add(500);
+
+list.Remove(10);
