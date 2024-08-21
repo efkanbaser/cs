5cea3e0c9aaafcb75f17459f62828b05765f584d
Efkan Başer <118986513+efkanbaser@users.noreply.github.com>
2024-08-21 21:19:51 +0300

    Add files via upload

---
+++ string_methods.cs
@@ -0,0 +1,32 @@
+﻿string name = "Efkan";
+string msg = "";
+for (int i = 0; i < name.Length; i++)
+{
+    msg += name[i] + "\n";
+}
+
+bool b = name.Contains("E");
+
+char[] arr = new char[name.Length];
+name.CopyTo(arr);
+name.CopyTo(0, arr, 0, name.Length);
+
+b = name.StartsWith("D");
+b = name.EndsWith("an");
+
+// Starting from index 4, it removes 2 characters.
+string nameClean = name.Remove(2, 2);
+
+string nameReplaced = name.Replace("Efk", "Serk");
+
+string splitMe = "Ali,Ahmet,Ayşe,Selim";
+string[] names = splitMe.Split(',');
+for (int i = 0; i < names.Length; i++)
+{
+    msg += names[i] + "\n";
+}
+
+
+
+
+
