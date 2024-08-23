463b9d9bd0a76a14d6e3279ea88e0409d6609b17
Efkan Başer <118986513+efkanbaser@users.noreply.github.com>
2024-08-23 21:13:50 +0300

    Add files via upload

---
+++ math_methods.cs
@@ -0,0 +1,21 @@
+﻿int a = Math.Abs(-10);
+double b = Math.Acos(0.48);
+b = Math.BigMul(50000, 50000); // Returns a long value just in case int*int returns bigger than sizeofint
+b = Math.Ceiling(15.1); // Her zaman yukarı yuvarlar
+b = Math.Floor(15.9); // Her zaman aşağı yuvarlar
+b = Math.Round(15.96, 1); // Virgülden sonra kaç karakter istediğini seçersin, sonra da son karaktere göre yuvarlama yapar.
+
+b = Math.Min(500, 100); // Verilen sayıdan en küçük olanı verir
+b = Math.Max(500, 100); // Verilen sayıdan en büyük olanı verir
+b = Math.Pow(2, 3); // 2nin 3. kuvvetini alır
+
+Math.DivRem(150, 7, out a);
+b = Math.PI;
+b = Math.E;
+
+b = Math.Sqrt(16);
+b = Math.Exp(2);
+b = Math.Log(10); // ln10, not actually log10
+b = Math.Log10(10); 
+
+
