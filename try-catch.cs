bc4f4782bfd1a8f48b15fbfa144cf501937551ed
Efkan Başer <118986513+efkanbaser@users.noreply.github.com>
2024-08-26 20:48:22 +0300

    Add files via upload

---
+++ try-catch.cs
@@ -0,0 +1,58 @@
+﻿//try
+//{
+//    string Sayi = "Ahmet";
+//    int sayi = Convert.ToInt32(Sayi);
+//}
+//catch (Exception e)
+//{
+//    Console.WriteLine("Hata oluştu\n" + e.Message + "\n" + e.HelpLink + "\n" + e.StackTrace);
+//}
+
+
+try
+{
+    string Sayi = "Ahmet";
+    int sayi = Convert.ToInt32(Sayi);
+    int sonuc = 150 / sayi;
+}
+catch (FormatException e)
+{
+    Console.WriteLine("Lüften bir sayı giriniz");
+    // Kendine mail atabilirisn
+    // Loglama yapabilirsin
+}
+catch (DivideByZeroException e)
+{
+    Console.WriteLine("Lütfen sıfırdan başka bir sayı giriniz");
+}
+catch (OverflowException e)
+{
+    Console.WriteLine("Daha küçük bir sayı giriniz");
+}
+catch (Exception e)
+{
+    Console.WriteLine("Hata oluştu. Sistem yöneticisine başvurunuz. Hata kodu 50105");
+    throw;
+}
+finally
+{
+    // This works no matter what you do
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
